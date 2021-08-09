f90e8546ed2d6c5bf30ca7b71760dba54b7a983d
madityafr <madityafr3@gmail.com>
2021-08-09 08:08:10 +0700

    05 tipe data map

---
+++ 05_tipeDataMap.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	person := map[string]string{
+		"name": "adit",
+		"address": "cimahi",
+	}
+
+	person["gender"] = "male"
+	
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+	fmt.Println(len(person))
+	
+	delete(person, "gender")
+	fmt.Println(person)
+}
--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 2. Operasi Matematika
 3. Tipe Data Array
 4. Tipe Data Slice
+5. Tipe Data Map
 
 Referensi:  [Programmer Zaman Now](https://www.youtube.com/ProgrammerZamanNow)
